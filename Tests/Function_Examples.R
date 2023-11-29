9fad5d6bddff822c456c76832d57dd7c03f6a35a
David Pearce <davepearce@tamu.edu>
2023-11-29 12:26:56 -0600

    updated tests from ?? to ?

--- Tests/Function_Examples.R
+++ Tests/Function_Examples.R
@@ -2,37 +2,37 @@
 all_functions <- ls("package:AvoidAttract", all.names = TRUE)
 all_functions
 
-??KScams_dat
-??KScams_cov
-??KScams_doy
+?KScams_dat
+?KScams_cov
+?KScams_doy
 
 
 data("KScams_dat")
 KScams_dat$DateTime  <- as.POSIXct(KScams_dat$DateTime ,  tryFormats = "%m/%d/%Y %H:%M:%OS") # this is very important step
 
 # Example use
-??T1
+?T1
 
 T1_test <- T1(data = KScams_dat, speciesA ="White-Tailed Deer", speciesB = "Coyote",
                 species_col = "Common_name", datetime_col = "DateTime", site_col ="Site",
                 unitTime = "hours")
 
 # Example use
-??T2
+?T2
 
 T2_test <- T2(data = KScams_dat, speciesA ="White-Tailed Deer", speciesB = "Coyote",
               species_col = "Common_name", datetime_col = "DateTime", site_col ="Site",
               unitTime = "hours")
 
 # Example use
-??T3
+?T3
 
 T3_test <- T3(data = KScams_dat, speciesA ="White-Tailed Deer",
               species_col = "Common_name", datetime_col = "DateTime", site_col ="Site",
               unitTime = "hours")
 
 # Example use
-??T4
+?T4
 
 T4_test <- T4(data = KScams_dat, speciesA = "White-Tailed Deer", speciesB = "Coyote",
               species_col = "Common_name", datetime_col = "DateTime", site_col ="Site",
@@ -40,7 +40,7 @@ T4_test <- T4(data = KScams_dat, speciesA = "White-Tailed Deer", speciesB = "Coy
 
 
 # Example usage
-??AAR
+?AAR
 
 AAR_test <- AAR(data = KScams_dat, speciesA = "White-Tailed Deer", speciesB = "Coyote",
                    species_col = "Common_name", datetime_col = "DateTime", site_col ="Site", unitTime = "hours")
