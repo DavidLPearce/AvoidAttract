457ec1e2e50449e02dcc0e8477f950aca1274806
David Pearce <davepearce@tamu.edu>
2023-11-28 23:40:44 -0600

    Added _dat to the data file before adding habitat covariates and day of year data to the package.

--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -1,6 +1,6 @@
 # data-raw/dataset.R
 
-KScams <- read.csv("data-raw/KansasCamera_data.csv")
+KScams_dat <- read.csv("data-raw/KansasCamera_data.csv")
 
-usethis::use_data(KScams, overwrite = TRUE)
+usethis::use_data(KScams_dat, overwrite = TRUE)
 
