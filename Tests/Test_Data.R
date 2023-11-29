b78916d8eee2e2b0d6a0de9a9dcdd1b0d7273cf0
David Pearce <davepearce@tamu.edu>
2023-11-28 22:37:53 -0600

    Commented out some of the objects that were used for function creation.

--- Tests/Test_Data.R
+++ Tests/Test_Data.R
@@ -1,28 +1,19 @@
 
-
-
-
-
-
-
-
-
-
-
+system.file(package='AvoidAttract')
 
 data("KScams")
 KScams$DateTime  <- as.POSIXct(KScams$DateTime ,  tryFormats = "%m/%d/%Y %H:%M:%OS") # this is very important step
 
 
-data = KScams
-species1 ="White-Tailed Deer"
-species2 = "Coyote"
-site_col = "Site"
-datetime_col = "DateTime"
-species_col = "Common_name"
-unitTime = "hours"
-
-site = 0
-year = 2020
-row = 1
+# data = KScams
+# species1 ="White-Tailed Deer"
+# species2 = "Coyote"
+# site_col = "Site"
+# datetime_col = "DateTime"
+# species_col = "Common_name"
+# unitTime = "hours"
+#
+# site = 0
+# year = 2020
+# row = 1
 
