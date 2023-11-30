9f3707739f006a20362c65e4483b14574ed443f4
David Pearce <davepearce@tamu.edu>
2023-11-30 14:51:55 -0600

    Working on automated test checks

--- tests/testthat/T2_test.R
+++ tests/testthat/T2_test.R
@@ -3,24 +3,24 @@ library(testthat)
 library(AvoidAttract)
 
 # Load the subset_data for testing
-subset_data <- readRDS("Tests/Test_data/test_data.rds")
+subset_data <- readRDS("tests/testthat/Test_data/test_data.rds")
 
 # expected result for T1 function
 expected_result <- list(
-  total_summary = as.difftime(177.2397, units = "hours"),
-  event_count = 13
+  total_summary = as.difftime(176.421, units = "hours"),
+  event_count = 3
 )
 
 # Test T1 function
-test_that("T1 function calculates time between detections correctly", {
-  result <- T1(data = subset_data, speciesA = "White-Tailed Deer", speciesB = "Coyote",
+test_that("T2 function calculates time between detections correctly", {
+  result <- T2(data = subset_data, speciesA = "White-Tailed Deer", speciesB = "Coyote",
                species_col = "Common_name", datetime_col = "DateTime", site_col = "Site")
 
   # Set a tolerance value
   tolerance <- 0.001
 
   # Check total summary with tolerance
-  expect_equal(result$total_summary, expected_result$total_summary, check.attributes = FALSE, tol = tolerance)
+  expect_equal(result$total_summary, expected_result$total_summary, ignore_attr = FALSE, tolerance = tolerance)
 
   # Check event count
   expect_equal(result$event_count, expected_result$event_count)
---
+++ tests/testthat/T3_test.R
@@ -0,0 +1,27 @@
+# Load necessary libraries
+library(testthat)
+library(AvoidAttract)
+
+# Load the subset_data for testing
+subset_data <- readRDS("tests/testthat/Test_data/test_data.rds")
+
+# expected result for T1 function
+expected_result <- list(
+  total_summary = as.difftime(114.9631, units = "hours"),
+  event_count = 113
+)
+
+# Test T1 function
+test_that("T3 function calculates time between detections correctly", {
+  result <- T3(data = subset_data, speciesA = "White-Tailed Deer", speciesB = "Coyote",
+               species_col = "Common_name", datetime_col = "DateTime", site_col = "Site")
+
+  # Set a tolerance value
+  tolerance <- 0.001
+
+  # Check total summary with tolerance
+  expect_equal(result$total_summary, expected_result$total_summary, ignore_attr = FALSE, tolerance = tolerance)
+
+  # Check event count
+  expect_equal(result$event_count, expected_result$event_count)
+})
