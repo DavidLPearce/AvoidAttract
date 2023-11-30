87b153e79a5d076f15480f70e5e30bb1f34705b3
David Pearce <davepearce@tamu.edu>
2023-11-30 14:20:54 -0600

    T1 function test is finished

---
+++ Tests/T1_test.R
@@ -0,0 +1,27 @@
+# Load necessary libraries
+library(testthat)
+library(AvoidAttract)
+
+# Load the subset_data for testing
+subset_data <- readRDS("tests/test_data.rds")
+
+# expected result for T1 function
+expected_result <- list(
+  total_summary = as.difftime(192.1822, units = "hours"),
+  event_count = 11
+)
+
+# Test T1 function
+test_that("T1 function calculates time between detections correctly", {
+  result <- T1(data = subset_data, speciesA = "White-Tailed Deer", speciesB = "Coyote",
+               species_col = "Common_name", datetime_col = "DateTime", site_col = "Site")
+
+  # Set a tolerance value
+  tolerance <- 0.001
+
+  # Check total summary with tolerance
+  expect_equal(result$total_summary, expected_result$total_summary, check.attributes = FALSE, tol = tolerance)
+
+  # Check event count
+  expect_equal(result$event_count, expected_result$event_count)
+})
